150210159ff236a97533b86c740745cc39b97edd
Arc891 <ezra.hulsman@gmail.com>
2024-08-10 16:40:20 +0200

    Got first few screen working; welcome, entry & login

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,94 @@
-use cursive::{view, Cursive};
-use cursive::views::{Button, Dialog, DummyView, EditView,
-                     LinearLayout, SelectView};
+// use std::thread;
+// use std::time::{Instant, Duration};
+// use cursive::logger::log;
+// use cursive::style::gradient::Linear;
+use cursive::Cursive;
 use cursive::traits::*;
+use cursive::view::Margins;
+use cursive::views::*;
+// use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, ResizedView, TextView};
+// use cursive_async_view::{AsyncState, AsyncView};
 
 fn main() {
     let mut siv = cursive::default();
 
-    let select = SelectView::<String>::new()
-        .on_submit(on_submit)
-        .with_name("select")
-        .fixed_size((10, 5));
-
-    let buttons = LinearLayout::vertical()
-        .child(Button::new("Add new", add_name))
-        .child(Button::new("Delete", delete_name))
-        .child(DummyView)
-        .child(Button::new("Quit", Cursive::quit));
-
-    siv.add_layer(Dialog::around(LinearLayout::horizontal()
-            .child(select)
-            .child(DummyView)
-            .child(buttons))
-        .title("Select a profile"));
+    let view = ResizedView::with_fixed_size((80, 15), 
+        Dialog::text("Welcome!")
+            .button("GO", login_menu));
+    
+    siv.add_layer(view);   
 
     siv.run();
 }
 
-fn on_submit(s: &mut Cursive, name: &str) {
+fn login_menu(s: &mut Cursive) {    
+    // let buttons = LinearLayout::horizontal()
+    //     .child(Button::new("Login", login))
+    //     .child(DummyView)
+    //     .child(Button::new("Register", register));
+    
     s.pop_layer();
-    s.add_layer(Dialog::text(format!("Name: {}\nAwesome: yes", name))
-        .title(format!("{}'s info", name))
-        .button("Quit", Cursive::quit))
+    s.add_layer(Dialog::text("Please login or register.")
+        .title("Welcome")
+        .button("Login", login)
+        .button("Register", register));
 }
 
-fn add_name(s: &mut Cursive) {
-    fn ok(s: &mut Cursive, name: &str) {
-        s.call_on_name("select", |view: &mut SelectView<String>| {
-            view.add_item_str(name);
-        });
-        s.pop_layer();
-    }
+fn login(s: &mut Cursive) { 
+    s.pop_layer();
 
-    s.add_layer(Dialog::around(EditView::new()
-            .on_submit(ok)
-            .with_name("name")
-            .fixed_width(10))
-        .title("Enter a new name")
-        .button("Ok", |s| {
-            let name = s.call_on_name("name", |view: &mut EditView| {
-                view.get_content()
-            }).unwrap();
-            ok(s, &name);
-        })
-        .button("Cancel", |s| {
-            s.pop_layer();
-        }));
+    let labels = LinearLayout::vertical()
+        .child(TextView::new("Username: "))
+        .child(TextView::new("Password: "));
+
+    let entries = LinearLayout::vertical()
+        .child(EditView::new()
+            .filler(" ")
+            // .on_submit(submit)
+            .with_name("username")
+            .fixed_width(15))
+        .child(EditView::new()
+            .secret()
+            .filler(" ")
+            // .on_submit(submit)
+            .with_name("password")
+            .fixed_width(15));
+
+    let login_view = LinearLayout::horizontal()
+        .child(labels)
+        .child(entries);
+
+    // let buttons = LinearLayout::horizontal()
+    //     .child(Button::new("Back", login_menu))
+    //     .child(Button::new("Submit", Cursive::quit))
+    //     .fixed_width(20);
+
+    s.add_layer(Dialog::around(login_view)
+        .title("Login")
+        .padding(Margins::lr(3, 3))
+        .button("Back", login_menu)
+        .button("Submit", submit));
 }
 
-fn delete_name(s: &mut Cursive) {
-    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
-    match select.selected_id() {
-        None => s.add_layer(Dialog::info("No name to remove")),
-        Some(focus) => {
-            select.remove_item(focus);
-        }
-    }
+fn register(s: &mut Cursive) {
+    s.pop_layer();
+
+}
+
+fn submit(s: &mut Cursive) {
+    let username = 
+        match s.call_on_name("username", |v: &mut EditView| { v.get_content().to_string() }) {
+            Some(s) => s,
+            None => String::from("")
+        };
+    let password = 
+        match s.call_on_name("password", |v: &mut EditView| { v.get_content().to_string() }) {
+            Some(s) => s,
+            None => String::from("")
+        };
+    
+        
+    s.pop_layer();
+    s.add_layer(TextView::new(format!("{} - {}", username, password)));
+    // s.quit();
 }
