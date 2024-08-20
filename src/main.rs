470a6bd1eb9eee00873f2de907c53bdf89330ccf
Arc891 <ezra.hulsman@gmail.com>
2024-08-20 23:45:22 +0200

    Added chat window, better register checks, overall improvements & refactor to 2 space tabs

--- src/main.rs
+++ src/main.rs
@@ -11,253 +11,299 @@ use cursive::views::*;
 // mod server;
 
 fn main() {
-    let mut siv = cursive::default();
+  let mut siv = cursive::default();
 
-    // Read logo from assets/logo_full.txt
-    let logo: &str = include_str!("../assets/logo_full.txt");
+  // Read logo from assets/logo_full.txt
+  let logo: &str = include_str!("../assets/logo_full.txt");
 
-    let view = ResizedView::with_fixed_size((64, 11), 
-        Dialog::text(logo)
-            .button("GO", login_menu));
-    
-    siv.add_layer(view);   
+  let view = ResizedView::with_fixed_size((64, 11), 
+    Dialog::text(logo)
+      .button("GO", login_menu));
+  
+  siv.add_layer(view);   
 
-    siv.run();
+  siv.run();
 }
 
-fn login_menu(s: &mut Cursive) {    
-    // let buttons = LinearLayout::horizontal()
-    //     .child(Button::new("Login", login))
-    //     .child(DummyView)
-    //     .child(Button::new("Register", register));
-    
-    s.pop_layer();
-    s.add_layer(Dialog::text("Please login or register.")
-        .title("Welcome")
-        .button("Login", login)
-        .button("Register", register));
+fn login_menu(s: &mut Cursive) {  
+  // let buttons = LinearLayout::horizontal()
+  //   .child(Button::new("Login", login))
+  //   .child(DummyView)
+  //   .child(Button::new("Register", register));
+  
+  s.pop_layer();
+  s.add_layer(Dialog::text("Please login or register.")
+    .title("Welcome")
+    .button("Login", login)
+    .button("Register", register));
 }
 
 fn login(s: &mut Cursive) { 
-    s.pop_layer();
+  s.pop_layer();
 
-    let labels = LinearLayout::vertical()
-        .child(TextView::new("Username: "))
-        .child(TextView::new("Password: "));
+  let labels = LinearLayout::vertical()
+    .child(TextView::new("Username: "))
+    .child(TextView::new("Password: "));
 
-    let entries = LinearLayout::vertical()
-        .child(EditView::new()
-            .filler(" ")
-            .on_submit(submit_login_with_arg)
-            .with_name("username")
-            .fixed_width(15))
-        .child(EditView::new()
-            .secret()
-            .filler(" ")
-            .on_submit(submit_login_with_arg)
-            .with_name("password")
-            .fixed_width(15));
+  let entries = LinearLayout::vertical()
+    .child(EditView::new()
+      .filler(" ")
+      .on_submit(submit_login_with_arg)
+      .with_name("username")
+      .fixed_width(15))
+    .child(EditView::new()
+      .secret()
+      .filler(" ")
+      .on_submit(submit_login_with_arg)
+      .with_name("password")
+      .fixed_width(15));
 
-    let login_view = LinearLayout::horizontal()
-        .child(labels)
-        .child(entries);
+  let login_view = LinearLayout::horizontal()
+    .child(labels)
+    .child(entries);
 
-    // let buttons = LinearLayout::horizontal()
-    //     .child(Button::new("Back", login_menu))
-    //     .child(Button::new("Submit", Cursive::quit))
-    //     .fixed_width(20);
+  // let buttons = LinearLayout::horizontal()
+  //   .child(Button::new("Back", login_menu))
+  //   .child(Button::new("Submit", Cursive::quit))
+  //   .fixed_width(20);
 
-    s.add_layer(Dialog::around(login_view)
-        .title("Login")
-        .padding(Margins::lr(3, 3))
-        .button("Back", login_menu)
-        .button("Register", register)
-        .button("Submit", submit_login));
+  s.add_layer(Dialog::around(login_view)
+    .title("Login")
+    .padding(Margins::lr(3, 3))
+    .button("Back", login_menu)
+    .button("Register", register)
+    .button("Submit", submit_login));
 }
+
 
 fn register(s: &mut Cursive) {
-    s.pop_layer();
-    
-    let labels = LinearLayout::vertical()
-        .child(TextView::new("Username: "))
-        .child(TextView::new("Password: "))
-        .child(TextView::new("Confirm password: ")); 
-    
-    let entries = LinearLayout::vertical()
-        .child(EditView::new()
-            .filler(" ")
-            .on_submit(submit_register_with_arg)
-            .with_name("username")
-            .fixed_width(15))
-        .child(EditView::new()
-            .secret()
-            .filler(" ")
-            .on_submit(submit_register_with_arg)
-            .with_name("password")
-            .fixed_width(15))
-        .child(EditView::new()
-            .secret()
-            .filler(" ")
-            .on_submit(submit_register_with_arg)
-            .with_name("password_confirm")
-            .fixed_width(15));
+  s.pop_layer();
+  
+  let labels = LinearLayout::vertical()
+    .child(TextView::new("Username: "))
+    .child(TextView::new("Password: "))
+    .child(TextView::new("Confirm password: ")); 
+  
+  let entries = LinearLayout::vertical()
+    .child(EditView::new()
+      .filler(" ")
+      .on_submit(submit_register_with_arg)
+      .with_name("username")
+      .fixed_width(15))
+    .child(EditView::new()
+      .secret()
+      .filler(" ")
+      .on_submit(submit_register_with_arg)
+      .with_name("password")
+      .fixed_width(15))
+    .child(EditView::new()
+      .secret()
+      .filler(" ")
+      .on_submit(submit_register_with_arg)
+      .with_name("password_confirm")
+      .fixed_width(15));
 
-        let register_view = LinearLayout::horizontal()
-            .child(labels)
-            .child(entries);
+    let register_view = LinearLayout::horizontal()
+      .child(labels)
+      .child(entries);
 
-    s.add_layer(Dialog::around(register_view)
-        .title("Register")
-        .padding(Margins::lr(3, 3))
-        .button("Back", login_menu)
-        .button("Login", login)
-        .button("Submit", submit_register));
+  s.add_layer(Dialog::around(register_view)
+    .title("Register")
+    .padding(Margins::lr(3, 3))
+    .button("Back", login_menu)
+    .button("Login", login)
+    .button("Submit", submit_register));
 }
 
-fn check_register(username: &str, password: &str, password_confirm: &str) -> bool {
-    if username == "" { return false; }
-    if password == password_confirm && password != "" { return true; }
-    false 
+fn check_register(username: &str, password: &str, password_confirm: &str) -> u32 {
+  if username == "" { 1 }
+  else if password == password_confirm && password != "" { 0 } 
+  else { 2 }
 }
 
 fn submit_register_with_arg(s: &mut Cursive, _: &str) {
-    submit_register(s);
+  submit_register(s);
 }
 
 fn submit_register(s: &mut Cursive) {
-    let username = 
-        match s.call_on_name("username", |v: &mut EditView| { v.get_content().to_string() }) {
-            Some(s) => s,
-            None => String::from("")
-        };
-    let password = 
-        match s.call_on_name("password", |v: &mut EditView| { v.get_content().to_string() }) {
-            Some(s) => s,
-            None => String::from("")
-        };
-    let password_confirm = 
-        match s.call_on_name("password_confirm", |v: &mut EditView| { v.get_content().to_string() }) {
-            Some(s) => s,
-            None => String::from("")
-        };
+  let username = 
+    match s.call_on_name("username", |v: &mut EditView| { v.get_content().to_string() }) {
+      Some(s) => s,
+      None => String::from("")
+    };
+  let password = 
+    match s.call_on_name("password", |v: &mut EditView| { v.get_content().to_string() }) {
+      Some(s) => s,
+      None => String::from("")
+    };
+  let password_confirm = 
+    match s.call_on_name("password_confirm", |v: &mut EditView| { v.get_content().to_string() }) {
+      Some(s) => s,
+      None => String::from("")
+    };
 
-    if check_register(&username, &password, &password_confirm) == false {
-        s.add_layer(Dialog::info("Invalid username or password."));
-        return;
-    }
 
-    main_menu(s);
+  let register_status = check_register(&username, &password, &password_confirm);
+  if register_status > 0 {
+    let register_error = match register_status {
+      1 => "Invalid username or already taken.",
+      2 => "Passwords do not match.",
+      0|3_u32..=u32::MAX => "Unknown error.",
+    };
+    s.add_layer(Dialog::info(register_error));
+    return;
+  }
+
+  main_menu(s);
 }
-
-
 
 
 fn check_login(username: &str, password: &str) -> bool {
-    if username != "" && password != "" {
-        return true;
-    } else {
-        return false;
-    }
+  return username != "" && password != "";
 }
 
 fn submit_login_with_arg(s: &mut Cursive, _: &str) {
-    submit_login(s);
+  submit_login(s);
 }
 
 fn submit_login(s: &mut Cursive) {
-    let username = 
-        match s.call_on_name("username", |v: &mut EditView| { v.get_content().to_string() }) {
-            Some(s) => s,
-            None => String::from("")
-        };
-    let password = 
-        match s.call_on_name("password", |v: &mut EditView| { v.get_content().to_string() }) {
-            Some(s) => s,
-            None => String::from("")
-        };
+  let username = 
+    match s.call_on_name("username", |v: &mut EditView| { v.get_content().to_string() }) {
+      Some(s) => s,
+      None => String::from("")
+    };
+  let password = 
+    match s.call_on_name("password", |v: &mut EditView| { v.get_content().to_string() }) {
+      Some(s) => s,
+      None => String::from("")
+    };
+  
+  if check_login(&username, &password) == false {
+    s.add_layer(Dialog::info("Invalid username or password."));
+    return;
+  }
     
-    if check_login(&username, &password) == false {
-        s.add_layer(Dialog::info("Invalid username or password."));
-        return;
-    }
-        
-    main_menu(s);
+  main_menu(s);
 }
 
 
 fn main_menu(s: &mut Cursive) {
-    s.pop_layer();
-    let terminal_input = EditView::new()
-        .filler(" ")
-        .on_submit(terminal_command)
-        .with_name("input")
-        .fixed_width(80);
+  s.pop_layer();
+  let terminal_input = EditView::new()
+    .filler(" ")
+    .on_submit(terminal_command)
+    .with_name("input")
+    .fixed_width(80);
 
-    let terminal_prefix = TextView::new("$ ")
-        .fixed_width(2);
+  let terminal_prefix = TextView::new("$ ")
+    .fixed_width(2);
 
-    let terminal = LinearLayout::vertical()
-        .child(TextView::new("-- Terminal --")
-            .with_name("output")
-            .scrollable()
-            .fixed_height(20))
-        .child(LinearLayout::horizontal()
-            .child(terminal_prefix)
-            .child(terminal_input));
+  let terminal = LinearLayout::vertical()
+    .child(TextView::new("-- Terminal --\n")
+      .with_name("output")
+      .scrollable()
+      .fixed_height(20))
+    .child(LinearLayout::horizontal()
+      .child(terminal_prefix)
+      .child(terminal_input));
 
-    let chats = vec![("Chat 1", "Chat 1 description"),
-        ("Chat 2", "Chat 2 description"),
-        ("Chat 3", "Chat 3 description")];
-    let select = SelectView::new()
-        .with_all(std::iter::repeat(chats).take(1).flatten())
-        .on_submit(|s, item: &str| s.add_layer(Dialog::info(format!("Selected: {}", item))))
-        .with_name("chats");
+  let chats = vec![("Chat 1", "Chat 1 description"),
+    ("Chat 2", "Chat 2 description"),
+    ("Chat 3", "Chat 3 description")];
+  let select = SelectView::new()
+    .with_all(std::iter::repeat(chats).take(1).flatten())
+    //.on_submit(|s, item: &str| s.add_layer(Dialog::info(format!("Selected: {}", item))))
+    .on_submit(|s, item: &str| chat(s, item))
+    .with_name("chats");
 
-    let main_menu = LinearLayout::horizontal()
-        .child(select)
-        .child(DummyView)
-        .child(terminal);
+  let main_menu = LinearLayout::horizontal()
+    .child(select)
+    .child(DummyView)
+    .child(terminal);
 
-    s.add_layer(Dialog::around(main_menu)
-        .title("Main menu")
-        .button("Logout", login_menu)
-        .button("Quit", Cursive::quit));
+  s.add_layer(Dialog::around(main_menu)
+    .title("Main menu")
+    .button("Logout", login_menu)
+    .button("Quit", Cursive::quit));
 
 }
 
+fn chat(s: &mut Cursive, chat_title: &str) {
+  s.pop_layer();
+  let chat_input = EditView::new()
+    .filler(" ")
+    .on_submit(chat_message)
+    .with_name("input")
+    .fixed_width(80);
+
+  let chat_prefix = TextView::new("> ")
+    .fixed_width(2);
+
+  let chat = LinearLayout::vertical()
+    .child(TextView::new(format!("-- {chat_title} --\n"))
+      .with_name("output")
+      .scrollable()
+      .fixed_height(20))
+    .child(LinearLayout::horizontal()
+      .child(chat_prefix)
+      .child(chat_input));
+
+  let chat_menu = LinearLayout::horizontal()
+    .child(chat);
+
+  s.add_layer(Dialog::around(chat_menu)
+    .title("Chat")
+    .button("Back", main_menu)
+    .button("Quit", Cursive::quit));
+}
+
+fn chat_message(s: &mut Cursive, message: &str) {
+  if message.starts_with("!") {
+    terminal_command(s, &message[1..]);
+    return;
+  }
+  
+  s.call_on_name("input", |v: &mut EditView| {
+    v.set_content("");
+  });
+
+  s.call_on_name("output", |v: &mut TextView| {
+    if message != "" {
+      v.append(format!("{}\n", message));
+    }
+  });
+}
 
 fn terminal_command(s: &mut Cursive, command: &str) {
-    let output = 
-        match command {
-        "h"|"help" => { "Available commands: [h]elp, [q]uit, [l]ist, [cl]ear, join, create" },
-        "q"|"quit" => { s.quit(); "Goodbye!" },
-        "l"|"list" => { 
-            let chats = s.call_on_name("chats", 
-                |v: &mut SelectView<&str>| { v.iter().map(|(s, _)| String::from(s)).collect::<Vec<String>>()
-            });
-            match chats {
-                Some(c) => &format!("Available chats: {:?}", c),
-                None => "No chats available."
-            }
-        },
-        "cl"|"clear" => { 
-            s.call_on_name("output", |v: &mut TextView| { v.set_content(""); });
-            "Output cleared."
-        },
-        "join" => { "Joining chat..." },
-        "create" => { "Creating chat..." },
-        _ => { "Unknown command. Type 'help' for a list of commands." }
-    };
+  let output = 
+    match command {
+    "h"|"help" => { "Available commands: [h]elp, [q]uit, [l]ist, [cl]ear, join, create" },
+    "q"|"quit" => { s.quit(); "Goodbye!" },
+    "l"|"list" => { 
+      let chats = s.call_on_name("chats", 
+        |v: &mut SelectView<&str>| { v.iter().map(|(s, _)| String::from(s)).collect::<Vec<String>>()
+      });
+      match chats {
+        Some(c) => &format!("Available chats: {:?}", c),
+        None => "No chats available."
+      }
+    },
+    "cl"|"clear" => { 
+      s.call_on_name("output", |v: &mut TextView| { v.set_content("-- Terminal --\n"); });
+      ""
+    },
+    "join" => { "Joining chat..." },
+    "create" => { "Creating chat..." },
+    _ => { "Unknown command. Type 'help' for a list of commands." }
+  };
 
-    s.call_on_name("input", |v: &mut EditView| {
-        v.set_content("");
-    });
+  s.call_on_name("input", |v: &mut EditView| {
+    v.set_content("");
+  });
 
-    s.call_on_name("output", |v: &mut TextView| {
-        let pre = match v.get_content().into() {
-            Some(s) => { if s.is_empty() { "" } else { "\n" } },
-            None => ""
-        };
-        v.append(format!("{}$ {}\n{}", pre, command, output));
-    });
+  s.call_on_name("output", |v: &mut TextView| {
+    if output != "" {
+      v.append(format!("$ {}\n{}\n", command, output));
+    }
+  });
 }
